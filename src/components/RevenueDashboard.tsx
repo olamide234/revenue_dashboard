67770ff2dcb1d63ce40d04c1130bbf19e57e1932
Olamide Tofade <tofade.olamide2@gmail.com>
2024-04-24 23:56:29 +0100

    feat: graph texts

--- src/components/RevenueDashboard.tsx
+++ src/components/RevenueDashboard.tsx
@@ -1,4 +1,5 @@
 'use client';
+import CautionIcon from '@app/assets/svg/CautionIcon';
 import DownloadIcon from '@app/assets/svg/DownloadIcon';
 import DropdownArrowIcon from '@app/assets/svg/DropdownArrowIcon';
 import { IFilter } from '@app/types';
@@ -6,13 +7,69 @@ import React, { Dispatch, SetStateAction } from 'react';
 
 export default function RevenueDashboard({
   setfilterDialog,
-  filters
+  filters,
 }: {
   setfilterDialog: Dispatch<SetStateAction<boolean>>;
   filters: IFilter;
 }) {
   return (
     <div className="ml-20 mr-36 w-full">
+      <div className="my-16 flex gap-72">
+        <div>
+          <div className="flex items-center gap-16">
+            <div>
+              <div className="mb-2 text-sm font-medium text-[#56616B]">
+                Available Balance
+              </div>
+              <div className="text-4xl font-bold text-[#131316]">
+                USD 120,500.00
+              </div>
+            </div>
+            <button className="rounded-[100px] bg-[#131316] px-7 py-3.5 font-semibold text-white">
+              Withdraw
+            </button>
+          </div>
+          <></>
+        </div>
+        <div className="flex w-[16.94rem] min-w-fit flex-col gap-8">
+          <div>
+            <div className="mb-2 flex items-center justify-between gap-2 text-sm font-medium text-[#56616B]">
+              Ledger Balance
+              <CautionIcon />
+            </div>
+            <div className="text-[1.75rem] font-bold text-[#131316]">
+              USD 0.00
+            </div>
+          </div>
+          <div>
+            <div className="mb-2 flex items-center justify-between gap-2 text-sm font-medium text-[#56616B]">
+              Total Payout
+              <CautionIcon />
+            </div>
+            <div className="text-[1.75rem] font-bold text-[#131316]">
+              USD 55,080.00
+            </div>
+          </div>
+          <div>
+            <div className="mb-2 flex items-center justify-between gap-2 text-sm font-medium text-[#56616B]">
+              Total Revenue
+              <CautionIcon />
+            </div>
+            <div className="text-[1.75rem] font-bold text-[#131316]">
+              USD 175,580.00
+            </div>
+          </div>
+          <div>
+            <div className="mb-2 flex items-center justify-between gap-2 text-sm font-medium text-[#56616B]">
+              Pending Payout
+              <CautionIcon />
+            </div>
+            <div className="text-[1.75rem] font-bold text-[#131316]">
+              USD 0.00
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex justify-between gap-6 border-b border-b-[#EFF1F6] pb-6">
         <div>
           <p className="text-2xl font-bold text-[#131316]">24 Transactions</p>
@@ -21,7 +78,10 @@ export default function RevenueDashboard({
           </p>
         </div>
         <div className="flex gap-3 font-semibold text-[#131316]">
-          <button onClick={()=>setfilterDialog(true)} className="flex items-center gap-1 rounded-[100px] bg-[#EFF1F6] py-3 pl-[30px] pr-5">
+          <button
+            onClick={() => setfilterDialog(true)}
+            className="flex items-center gap-1 rounded-[100px] bg-[#EFF1F6] py-3 pl-[30px] pr-5"
+          >
             Filter
             <DropdownArrowIcon />
           </button>
